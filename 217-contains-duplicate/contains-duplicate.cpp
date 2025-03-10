ecdbd6208bd04392693d62efeab38d188d470499
spaceCrap <146406262+spaceCrap@users.noreply.github.com>
2025-03-10 15:22:59 +0530

    Time: 60 ms (17.20%) | Memory: 77.3 MB (6.27%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        set<int> st(nums.begin(),nums.end());
+        return st.size()!=nums.size();
+        
+    }
+};
