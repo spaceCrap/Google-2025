1f8ee95bc7ab44a73558c5ec882096ff326895ea
spaceCrap <146406262+spaceCrap@users.noreply.github.com>
2025-03-17 09:42:28 +0530

    Time: 33 ms (51.65%) | Memory: 71.2 MB (47.04%) - LeetSync

--- 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.cpp
+++ 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.cpp
@@ -1,30 +1,31 @@
 class Solution {
-
-private:
     bool dfs(vector<vector<char>>& grid, vector<vector<int>>& visited,
-             int level, int row, int col, pair<int, int> prevCord) {
-        char currentVal = grid[row][col];
+             char initialColor, pair<int, int> currentNode,
+             pair<int, int> prevNode, int level) {
+        auto [row, col] = currentNode;
         visited[row][col] = 1;
         int deltaRow[] = {-1, 0, 1, 0};
         int deltaCol[] = {0, 1, 0, -1};
-        for (int i = 0; i < 4; i++) {
-            int nRow = row + deltaRow[i];
-            int nCol = col + deltaCol[i];
+
+        for (int neighbour = 0; neighbour < 4; neighbour++) {
+            int nRow = row + deltaRow[neighbour];
+            int nCol = col + deltaCol[neighbour];
 
             if (nRow >= 0 and nRow < grid.size() and nCol >= 0 and
-                nCol < grid[0].size() && grid[nRow][nCol] == currentVal) {
-                if (visited[nRow][nCol] != 1) {
-                    if (dfs(grid, visited, level + 1, nRow, nCol, {row, col}) ==
-                        true) {
+                nCol < grid[0].size() and grid[nRow][nCol] == initialColor) {
+                if (!visited[nRow][nCol]) {
+                    if (dfs(grid, visited, initialColor, {nRow, nCol},
+                            currentNode, level + 1) == true) {
                         return true;
                     }
-                  
-                } else if (visited[nRow][nCol] == 1 and
-                           !(nRow == prevCord.first &&
-                             nCol == prevCord.second) and
-                           level >= 4) {
-                    return true;
-                }
+
+                } else if (visited[nRow][nCol] and
+                           !(nRow == prevNode.first and
+                             nCol == prevNode.second) and
+                           level >= 4)
+                       {
+                        return true;
+                       }
             }
         }
         return false;
@@ -34,13 +35,16 @@ public:
     bool containsCycle(vector<vector<char>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
+
         vector<vector<int>> visited(m, vector<int>(n, 0));
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (!visited[i][j]) {
+        for (int row = 0; row < m; row++) {
+            for (int col = 0; col < n; col++) {
+                if (!visited[row][col]) {
 
-                    if (dfs(grid, visited, 1, i, j, {-1, -1}) == true) {
+                    char initialColor = grid[row][col];
+                    if (dfs(grid, visited, initialColor, {row, col}, {-1, -1},
+                            1) == true) {
                         return true;
                     }
                 }
@@ -49,3 +53,10 @@ public:
         return false;
     }
 };
+
+/**
+check if visited
+- no -> run dfs on same element (increase counter);
+- yes -> check if same as prev element & count is 4 - yes return true;
+- check all neighbours
+ */
